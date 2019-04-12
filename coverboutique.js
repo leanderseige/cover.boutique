31856678cf2a4e88c1494da005f5b8984d905050
Leander Seige <leander@seige.name>
2019-04-12 19:34:15 +0200

    first real cover

--- coverboutique.js
+++ coverboutique.js
@@ -246,7 +246,8 @@ function coverboutique(config) {
         var img = document.createElement('img');
         img.crossOrigin = "Anonymous";
 
-        var imgurl_mask = "http://localhost:8000/images/test2.png"
+        // var imgurl_mask = "http://localhost:8000/images/test2.png";
+        var imgurl_mask = "http://localhost:8000/images/Samsung-J5_2017.png";
         var img_mask = document.createElement('img');
         img_mask.crossOrigin = "Anonymous";
 
